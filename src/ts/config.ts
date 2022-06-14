8476548275c9cb8af1a5a9dac471f151b467ca21
dennis0324 <dennis0324@naver.com>
2022-06-15 02:38:44 +0900

    config file composition change

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -1,15 +1,13 @@
 import * as dotenv from "dotenv"
-
+// import config from .env
 dotenv.config()
+
+// getting Discor_bot_token and scope
 const {DISCORD_BOT_TOKEN,SCOPE} = process.env;
 
-interface configFile {
-    DISCORD_BOT_TOKEN: string,
-    SCOPE_INT : number
-}
-const SCOPE_INT:number = parseInt(SCOPE); 
+// Changing Scope:string to Scope_Int:number
+const SCOPE_INT:number = parseInt(SCOPE);
+const DISCORD_TOKEN:string = DISCORD_BOT_TOKEN;
 
-const config:configFile = {
-    DISCORD_BOT_TOKEN,SCOPE_INT
-}
-export default config
+export {SCOPE_INT,DISCORD_TOKEN};
+// export default DISCORD_BOT_TOKEN;
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,6 @@
 // Require the necessary discord.js classes
 import {Client, Intents} from "discord.js"
-import config from "./config"
-
-
-
+import {DISCORD_TOKEN} from "./config"
 
 // Create a new client instance
 const client:Client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -15,4 +12,4 @@ client.once('ready', () => {
 
 
 // Login to Discord with your client's token
-client.login(config.DISCORD_BOT_TOKEN);
+client.login(DISCORD_TOKEN);
